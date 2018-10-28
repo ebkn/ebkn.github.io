f9a3b7a2e4433c75180f9fee94bbcda71a153f0d
ebkn <ktennis.mqekr12@gmail.com>
2018-10-29 02:44:45 +0900

    feat: create subTitle component

---
+++ src/atoms/subTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const SubTitle = ({ title }) => <StyledH2>{title}</StyledH2>;
+const StyledH2 = styled.h2`
+  margin: 0;
+  padding: 15px 0;
+  font-size: 22px;
+  color: #212121;
+  text-align: center;
+`;
+export default SubTitle;

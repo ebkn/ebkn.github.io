760b93636c5970c60b0ea581c69b36f591ed8ec9
ebkn <ktennis.mqekr12@gmail.com>
2018-10-28 22:12:26 +0900

    feat: create base component

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'top',
+    };
+  }
+
+  render() {
+    <div />
+  }
+}
---
+++ src/mobileApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class MobileApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'top',
+    };
+  }
+
+  render() {
+    <div />
+  }
+}

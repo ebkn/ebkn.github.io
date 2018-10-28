20f256e82fe4cb927da2e1bf4221d8f179b8f5ee
ebkn <ktennis.mqekr12@gmail.com>
2018-10-28 23:05:23 +0900

    feat: setup router

--- src/mobileApp.jsx
+++ src/mobileApp.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './mobile/main';
 import AboutMe from './mobile/aboutMe';
 import Skills from './mobile/skills';
 import Works from './mobile/works';
 import Jobs from './mobile/jobs';
+import NotFound from './mobile/notFound';
 
 export default class MobileApp extends React.Component {
   constructor(props) {
@@ -10,6 +13,15 @@ export default class MobileApp extends React.Component {
   };
 
   render() {
-    <div />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/aboutme" exact component={AboutMe} />
+        <Route path="/skills" exact component={Skills} />
+        <Route path="/works" exact component={Works} />
+        <Route path="/jobs" exact component={Jobs} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   }
 }

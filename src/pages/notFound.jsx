65ee8019bbbdf01cf0cd45a80c80d2c96c4e34ac
ebkn <ktennis.mqekr12@gmail.com>
2018-10-29 00:58:02 +0900

    feat: create styles for 404

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Title from "../atoms/title";
 
-const NotFound = () => {
-  return <h2>404 NOT FOUND</h2>;
-};
+const NotFound = () => (
+  <StyledWrapper>
+    <Title title="404 NOT FOUND" />
+  </StyledWrapper>
+);
+const StyledWrapper = styled.div`
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export default NotFound;

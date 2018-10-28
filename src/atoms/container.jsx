3d7b6e7ffd1a8ef13aff21325896ce815ec9d765
ebkn <ktennis.mqekr12@gmail.com>
2018-10-29 02:38:21 +0900

    feat: create container component

---
+++ src/atoms/container.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = ({ children }) => <StyledDiv>{children}</StyledDiv>;
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  box-sizing: border-box;
+  text-align: center;
+`;
+export default Container;

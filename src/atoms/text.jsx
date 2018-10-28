7e1ed1f4f53117775f388a5822e733917bffd515
ebkn <ktennis.mqekr12@gmail.com>
2018-10-29 00:15:26 +0900

    fix: text and title component style

--- src/atoms/text.jsx
+++ src/atoms/text.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-const Text = ({ text }) => <p>{text}</p>;
-export default styled(Text)`
-  font-size: 14px;
-  line-height: 14px;
+const Text = ({ text }) => <StyledP>{text}</StyledP>;
+const StyledP = styled.p`
+  font-size: 16px;
+  line-height: 16px;
   word-break: break-all;
   color: "#212121";
 `;
+export default Text;
--- src/atoms/title.jsx
+++ src/atoms/title.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-const Title = ({ title }) => <h1>{title}</h1>;
-export default styled(Title)`
+const Title = ({ title }) => <StyledH1>{title}</StyledH1>;
+const StyledH1 = styled.h1`
+  margin: 0;
   padding: 5px;
-  font-size: 18px;
+  font-size: 24px;
   color: "#212121";
 `;
+export default Title;

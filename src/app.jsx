860e46404c13df6f183ab98570e3501684eff881
ebkn <ktennis.mqekr12@gmail.com>
2018-10-29 03:13:34 +0900

    feat: rename from main to top component

--- src/app.jsx
+++ src/app.jsx
@@ -1,41 +1,34 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import styled from "styled-components";
-import Main from "./pages/main";
-import AboutMe from "./pages/aboutMe";
-import Skills from "./pages/skills";
-import Works from "./pages/works";
-import Jobs from "./pages/jobs";
-import NotFound from "./pages/notFound";
-import SideBar from "./molecules/sideBar";
-import Copyright from "./molecules/copyright";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import Top from './pages/top';
+import AboutMe from './pages/aboutMe';
+import Skills from './pages/skills';
+import Works from './pages/works';
+import Jobs from './pages/jobs';
+import NotFound from './pages/notFound';
+import SideBar from './molecules/sideBar';
+import Copyright from './molecules/copyright';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <GlobalStyledDiv>
-          <SideBar />
-          <StyledContainer>
-            <Switch>
-              <Route path="/" exact component={Main} />
-              <Route path="/aboutme" exact component={AboutMe} />
-              <Route path="/skills" exact component={Skills} />
-              <Route path="/works" exact component={Works} />
-              <Route path="/jobs" exact component={Jobs} />
-              <Route component={NotFound} />
-            </Switch>
-            <Copyright />
-          </StyledContainer>
-        </GlobalStyledDiv>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <GlobalStyledDiv>
+      <SideBar />
+      <StyledContainer>
+        <Switch>
+          <Route path="/" exact component={Top} />
+          <Route path="/aboutme" exact component={AboutMe} />
+          <Route path="/skills" exact component={Skills} />
+          <Route path="/works" exact component={Works} />
+          <Route path="/jobs" exact component={Jobs} />
+          <Route component={NotFound} />
+        </Switch>
+        <Copyright />
+      </StyledContainer>
+    </GlobalStyledDiv>
+  </BrowserRouter>
+);
+export default App;
 const GlobalStyledDiv = styled.div`
   display: flex;
   width: 100vw;
@@ -46,7 +39,5 @@ const StyledContainer = styled.main`
   width: 100%;
   box-sizing: border-box;
   padding: 30px 20px 20px 20px;
-  padding-left: {
-    ()=>isMobile() ? '0' : "30%";
-  };
+  padding-left: {() => isMobile()? '0' : "30%"};
 `;

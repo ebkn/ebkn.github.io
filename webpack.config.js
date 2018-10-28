f64aa0d54bc30199e68319fc2da903965104d26f
ebkn <ktennis.mqekr12@gmail.com>
2018-10-28 23:16:52 +0900

    yarn add normalize.css

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
     ],
   },
   resolve: {

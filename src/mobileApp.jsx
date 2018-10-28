53faf1b330d1dc34cb48073467180114abc3f8f3
ebkn <ktennis.mqekr12@gmail.com>
2018-10-28 22:54:20 +0900

    yarn add react-router-dom

--- src/mobileApp.jsx
+++ src/mobileApp.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import AboutMe from './mobile/aboutMe';
+import Skills from './mobile/skills';
+import Works from './mobile/works';
+import Jobs from './mobile/jobs';
 
 export default class MobileApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      page: 'top',
-    };
-  }
+  };
 
   render() {
     <div />

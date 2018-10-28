07bd62057e2ec910433c3bfd7775ed8c2d4606e8
ebkn <ktennis.mqekr12@gmail.com>
2018-10-28 22:02:57 +0900

    feat: create MobileApp and App

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { isMobile } from './utils';
+import MobileApp from './mobileApp';
+import App from './app';
 
 ReactDOM.render(
-  <div />,
+  isMobile() ? <MobileApp /> : <App />,
   document.getElementById('root')
 );

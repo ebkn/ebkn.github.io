5a4469e58bb6b972bf8283200ef13f9baf9e2c75
ebkn <ktennis.mqekr12@gmail.com>
2018-11-08 22:39:35 +0900

    feat: create badges molecule

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import Container from '../atoms/container';
 import ProfileIcon from '../atoms/profileIcon';
 import Title from '../atoms/title';
 import SocialIcons from '../molecules/socialIcons';
+import Badges from '../molecules/badges';
 import CenteredText from '../molecules/centeredText';
 import info from '../../info';
 
@@ -11,6 +12,7 @@ const Home = () => (
     <ProfileIcon />
     <Title title={info.name} />
     <SocialIcons />
+    <Badges />
     <CenteredText text={info.shortProfile} />
   </Container>
 );

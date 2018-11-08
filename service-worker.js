41b99cca07ee89d6ab75bccac6185ba682a193c9
ebkn <ktennis.mqekr12@gmail.com>
2018-11-09 01:55:07 +0900

    feat: add ServiceWorker

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+self.addEventListener('install', function() {
+  console.log('Service Worker install');
+});
+self.addEventListener('activate', function() {
+  console.log('Service Worker Activate');
+});
